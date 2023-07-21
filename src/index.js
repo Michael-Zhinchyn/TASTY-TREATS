3120d83ffdbb5896d0f8b62b284af1b8619f6919
AnnaHort <annahort3d@gmail.com>
2023-07-22 01:01:21 +0300

    add swiper card

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './js/header';
 import './js/order-modal';
 // import './js/all-categories';
 // import './js/popular-recipes';
-// import './js/swiper';
-// import './js/swiper-api';
+import './js/swiper';
+import './js/swiper-api';
 // import './js/search';
