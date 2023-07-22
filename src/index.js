611c63181323443d2b5ca95450d0297e94175e55
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-22 07:49:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './js/header';
 // import './js/recipe-modal';
 import './js/order-modal';
-// import './js/all-categories';
+import './js/all-categories';
 // import './js/popular-recipes';
 import './js/swiper';
 import './js/swiper-api';
