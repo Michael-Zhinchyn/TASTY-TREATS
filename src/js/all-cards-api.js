a258c7f09f32d4b6ad13031cb3984021f4b6b651
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-07-24 16:21:02 +0300

    all-cards&reset-filter#html&css

--- src/js/all-cards-api.js
+++ src/js/all-cards-api.js
@@ -56,7 +56,7 @@ function generateRecipeCard(recipe) {
         </div>
         <div class="card-bottom">
           <div class="card-rating-block">
-            <p class="card-rating">${recipe.rating}</p>
+            <p class="card-rating">${recipe.rating.toFixed(1)}</p>
             <div class="eating-stars">${generateStars(recipe.rating)}</div>
           </div>
           <button class="card-button" data-id="${
