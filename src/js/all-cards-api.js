d72b0f48f2721e78fda3975edb3ae0695616bafd
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-24 11:31:33 -0500

    update

--- src/js/all-cards-api.js
+++ src/js/all-cards-api.js
@@ -123,7 +123,7 @@ export async function getAllRecipes() {
         }
       });
     }
-    
+
     // -------------------------------------------------------------------------------------------------------------------------------------
 
     const seeRecipeButtons = recipesContainer.querySelectorAll('.card-button');
