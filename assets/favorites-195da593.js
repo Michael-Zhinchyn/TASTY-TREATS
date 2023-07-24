65d37b4b2282b4533c054b5e29ccecfe9e2def03
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-24 13:38:24 +0000

    Deploying to gh-pages from @ Michael-Zhinchyn/TASTY-TREATS@8b988fad5ea9fbb28d19490b48fed6120a03758b 🚀

--- assets/favorites-195da593.js
+++ assets/favorites-195da593.js
@@ -98,7 +98,7 @@ Notiflix needs to be appended to the "<`+v+'>" element, but you called it before
         </div>
         <div class="card-bottom">
           <div class="card-rating-block">
-            <p class="card-rating">${t.rating}</p>
+            <p class="card-rating">${t.rating.toFixed(1)}</p>
             <div class="eating-stars">${Qr(t.rating)}</div>
           </div>
           <button class="card-button" data-id="${t._id}">See recipe</button>
