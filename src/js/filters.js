e99e8cc35844aaae17aa13a1e958392a2ab25144
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-07-25 15:17:28 +0300

    Update filters.js

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,7 +1,7 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 import _ from 'lodash';
-const API_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes&perPage=48'
+const API_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes'
 const searchInput = document.querySelector('#id-input-search');
 const timeSelect = document.querySelector('#select-time');
 const areaSelect = document.querySelector('#select-area');
@@ -11,9 +11,7 @@ const recipeList = document.querySelector('.filter-card-set');
 async function fetchRecipes() {
   try {
     const response = await axios.get(`${API_URL}`);
-    const data = response.data;
-    console.log(response)
-    return data;
+    return response.data;
   } catch (error) {
     console.error("Нажаль по даному запиту рецепт не знайденний");
     return [];
@@ -29,12 +27,13 @@ const recipesRender = async () => {
 
   const allRecipe = await fetchRecipes();
 
-  const filtered = allRecipe.filter((recipe) => {
-    const isKeywordMatch = recipe.title.toLowerCase().includes(keyword);
-    const isTimeMatch = selectTime === "" || recipe.time <= parseInt(selectTime);
-    const isAreaMatch = selectArea === "" || recipe.area === selectArea;
-    const isIngredientsMatch = selectIngredients === "" || recipe.ingredients.includes(selectIngredients);
-    return isKeywordMatch && isTimeMatch && isAreaMatch && isIngredientsMatch;
+  const filtered = allRecipe.map(({ preview, title, description, _id }) => {
+    const isKeywordMatch = title.toLowerCase().includes(keyword);
+    // const isTimeMatch = selectTime === "" || recipe.time <= parseInt(selectTime);
+    // const isAreaMatch = selectArea === "" || recipe.area === selectArea;
+    // const isIngredientsMatch = selectIngredients === "" || recipe.ingredients.includes(selectIngredients);
+    // return isKeywordMatch && isTimeMatch && isAreaMatch && isIngredientsMatch;
+    return isKeywordMatch
   });
 
   recipeList.innerHTML = "";
