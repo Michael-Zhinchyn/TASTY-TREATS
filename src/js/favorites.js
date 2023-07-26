56de55ab0d5d7a4a6af8918e2a7b5175305a0597
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-25 21:35:51 -0500

    update favorites

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -2,35 +2,67 @@ import axios from 'axios';
 import { generateRecipeCard } from './all-cards-api';
 import { remove } from 'lodash';
 
+// Отримуємо дані з локального сховища
 let storedData = localStorage.getItem('inFavorite');
+
+// Ініціалізуємо масиви для ідентифікаторів рецептів та категорій
 let actualIDs = [];
+let addedCategories = [];
+
+// Якщо в локальному сховищі є дані, оброблюємо їх
 if (storedData) {
   let fullIDs = JSON.parse(storedData);
   actualIDs = fullIDs.map(id => id.replace('card-checkbox-', ''));
 }
 
 const API_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
+
+// Отримуємо елементи DOM
 const categoryBlock = document.getElementById('category-filter-div');
+const resetCategoryBtn = document.querySelector('.fav-category-fltr-btn')
+const paginationBar = document.querySelector('.pagination-bar')
+
+// Якщо існує панель навігації, ховаємо її
+if(paginationBar) {
+  paginationBar.style.display = 'none'
+}
+
+// Отримуємо контейнер для обраних рецептів
 export const favoritesContainer = document.querySelector('.favorite-card-list');
 
 async function addFavoriteRecipe(id) {
   try {
+    // Отримуємо рецепт з бекенду
     const response = await axios.get(`${API_URL}/${id}`);
     const recipe = response.data;
 
     const category = recipe.category;
-    const categoryMarkup = generateCategoryMarkup(category);
-    if (categoryBlock) {
-      categoryBlock.innerHTML += categoryMarkup;
+
+    // Перевіряємо, чи вже додана ця категорія
+    if (!addedCategories.includes(category)) {
+      // Якщо ні, додаємо її до масиву та створюємо розмітку
+      addedCategories.push(category);
+      const categoryMarkup = generateCategoryMarkup(category);
+      if (categoryBlock) {
+        categoryBlock.innerHTML += categoryMarkup;
+      }
     }
 
+    // Генеруємо картку рецепту
     const recipeCard = generateRecipeCard(recipe);
 
     if (favoritesContainer) {
       const messageBlock = document.querySelector('.block-for-empty');
+
+      // Ховаємо повідомлення та показуємо кнопку та панель навігації
       messageBlock.style.display = 'none';
+      resetCategoryBtn.style.display = 'flex'
+      paginationBar.style.display = 'flex'
+
+      // Додаємо нову картку рецепту
       favoritesContainer.innerHTML += recipeCard;
 
+      // Ініціалізуємо масив для вибраних чекбоксів
       const heartCheckBoxEl = document.querySelectorAll('.card-checkbox');
       let selectedHeartCheckBox = [];
 
@@ -95,6 +127,7 @@ async function addFavoriteRecipe(id) {
   }
 }
 
+// Завантажуємо обрані рецепти при завантаженні сторінки
 function loadFavoriteRecipes() {
   if (actualIDs) {
     actualIDs.forEach(id => {
@@ -103,8 +136,10 @@ function loadFavoriteRecipes() {
   }
 }
 
+// Функція для генерування розмітки для категорії
 function generateCategoryMarkup(category) {
   return `<button type="button" class="fav-category-btn">${category}</button>`;
 }
 
+// Завантажуємо рецепти при завантаженні сторінки
 loadFavoriteRecipes();
